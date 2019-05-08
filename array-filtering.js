8b1247dbba9ee45c721da26b78d3285808ee40a3
Juan Pablo Calle <jcalle94@hotmail.com>
2019-05-08 14:46:54 -0500

    adding files javascripting

---
+++ array-filtering.js
@@ -0,0 +1,8 @@
+'use strict'
+
+
+
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+var filtered = console.log(numbers.filter(function evenNumbers (number){
+    return number % 2 === 0
+}))
---
+++ function-arguments.js
@@ -0,0 +1,7 @@
+'use strict'
+
+function math(ar1, ar2, ar3){
+ return console.log(ar1 + ar2 * ar3)
+}
+
+math(53, 61, 67)
